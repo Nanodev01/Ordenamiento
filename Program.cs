b012f0f9d50646bf5272ac9b686be28e45d0ef63
Nanodev01 <andrerug@live.com>
2024-05-27 13:33:22 -0300

    inicio

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void bubble(int[] arr)
+        { 
+            // ordenamiento bubble sort
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (arr[j] < arr[i]) { 
+                        int aux = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = aux;
+                    }
+                }            
+            }
+        }
+        public static Int32[] randEntero(int size) {
+            Random rnd = new Random();
+            Int32[] arr = new Int32[size];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next();
+            
+            }
+            return arr;
+        }
+            
+            
+            
+            
+            
+            
+            
+            
+        static void Main(string[] args)  // Comienazo programa <------------------------------------
+        {
+
+            DateTime inicio = DateTime.Now;
+            Int32[] arr = randEntero(100000);
+            bubble(arr);
+            TimeSpan tiempo = DateTime.Now - inicio;
+
+            Console.WriteLine("Tiempo elapsado: {0:D1}:{1:D2}:{2:D3}", tiempo.Hours, tiempo.Minutes, tiempo.Seconds);
+            Console.ReadKey();
+           
+        }
+    }
+}
